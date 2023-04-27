143fdcd5f34397e35defa0bb0e6c7edd75edae02
runyu <njjry@yahoo.com>
2023-04-27 12:47:03 -0500

    update failover scripts

--- 2clusters/failover/step1_disaster.sh
+++ 2clusters/failover/step1_disaster.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 source env.sh
 /nfs/OCP-installer/oclogincluster $primary
-vrg=$(oc get vrg -n cpd-operators | awk '{print $1}' | sed -n '2p')
-kubectl patch vrg/$vrg --type=merge -p '{"spec":{"replicationState":"secondary"}}' -n cpd-operators
+vrg=$(oc get vrg -n ibm-common-services | awk '{print $1}' | sed -n '2p')
+kubectl patch vrg/$vrg --type=merge -p '{"spec":{"replicationState":"secondary"}}' -n ibm-common-services
 vrg=$(oc get vrg -n cpd-instance | awk '{print $1}' | sed -n '2p')
 kubectl patch vrg/$vrg --type=merge -p '{"spec":{"replicationState":"secondary"}}' -n cpd-instance
-sleep 600
-#/nfs/OCP-installer/oclogincluster $secondary
-#networkfence=$(oc get networkfences -n openshift-dr-system -o wide | awk '{print $1}' | sed -n '2p')
-#oc patch networkfences $networkfence -n openshift-dr-system -p '{"spec":{"fenceState":"Fenced"}}' --type=merge
-#sleep 5
-#./verify_fence.sh
-#sleep 5
-#mc tree cluster5 --insecure
+/nfs/OCP-installer/oclogincluster $secondary
+networkfence=$(oc get networkfences -n openshift-dr-system -o wide | awk '{print $1}' | sed -n '2p')
+oc patch networkfences $networkfence -n openshift-dr-system -p '{"spec":{"fenceState":"Fenced"}}' --type=merge
+sleep 5
+./verify_fence.sh
+sleep 5
+mc tree $primary --insecure
--- 2clusters/failover/step3_cleanup.sh
+++ 2clusters/failover/step3_cleanup.sh
@@ -2,5 +2,5 @@ source env.sh
 /nfs/OCP-installer/oclogincluster $primary
 cd ../
 ./delete-cpd-instance.sh primary
-#./delete-cpd-operators.sh primary
+./delete-cpd-operators.sh primary
 cd -
